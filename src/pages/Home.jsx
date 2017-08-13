da31bf5cd1dd23259b30c6fde5b6c94e06c15ebc
Arthmarellion <Arthmarellion@hotmail.com>
2017-08-13 01:56:06 -0400

    Feature: Add button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { SEARCH } from 'project-routes/path';
+import { Link } from 'react-router-dom';
+
 import * as BooksAPI from 'project-root/BooksAPI';
+import FontAwesome from 'react-fontawesome';
 import Shelf from 'project-root/components/Shelf';
 
 import classname from 'classname';
@@ -35,6 +39,11 @@ export default class DefaultLayout extends Component {
         {Object.keys(this.context.messages.Home.shelfs).map(key => (
           <Shelf key={key} books={this.filterBooks(key)} title={this.context.messages.Home.shelfs[key]} />
         ))}
+        <Link to={SEARCH}>
+          <div className={theme.add}>
+            <FontAwesome name="plus" size="lg" />
+          </div>
+        </Link>
       </div>
     );
   };
